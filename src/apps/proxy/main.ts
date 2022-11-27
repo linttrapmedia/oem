649130a568beba7a14ec3cbbeb088a9fae3875d6
Kevin Lint <kevin@kevinlint.com>
2022-11-26 17:34:26 -0800

    asdf

--- src/apps/proxy/main.ts
+++ src/apps/proxy/main.ts
@@ -3,10 +3,9 @@ import { render } from './lib';
 const num = NUMBER.set(1);
 
 render(
-  DIV.column(10).innerHtml(
-    H1.innerText('Counter'),
-    BUTTON.onClick(num.inc, 1).innerText('Increment'),
+  DIV.row(10).innerHtml(
+    BUTTON.onClick(num.dec, 2).innerText('–'),
     DIV.innerText(num.get),
-    BUTTON.onClick(num.dec, 2).innerText('Decrement'),
+    BUTTON.onClick(num.inc, 1).innerText('+'),
   ),
 );
