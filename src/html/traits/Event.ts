82ad5686f8e32f15da68b397bf4e839bde6620fb
Kevin Lint <kevin@concreit.com>
2024-01-30 08:54:24 -0800

    fix event listener sig

--- src/html/traits/Event.ts
+++ src/html/traits/Event.ts
@@ -1,5 +1,5 @@
 export const useEvent =
-  (evt: keyof GlobalEventHandlersEventMap) =>
-  (el: HTMLElement, cb: () => any, condition?: (() => boolean) | boolean) => {
+  <E extends keyof GlobalEventHandlersEventMap>(evt: E) =>
+  (el: HTMLElement, cb: (evt: GlobalEventHandlersEventMap[E]) => any, condition?: (() => boolean) | boolean) => {
     (typeof condition === 'function' ? condition() : condition ?? true) ? el.addEventListener(evt, cb) : null;
   };
