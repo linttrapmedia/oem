4ef04eaccfc59a9809c402825c7862db13c31f85
Kevin Lint <kevin@concreit.com>
2025-02-06 12:54:59 -0800

    Adds undefined as valid children type, filters them out

--- src/html/traits/InnerHTML.ts
+++ src/html/traits/InnerHTML.ts
@@ -4,17 +4,25 @@ type UseInnerHTMLProps<T> = {
   state: StateType<T>;
 };
 
+type Children =
+  | string
+  | number
+  | HTMLElement
+  | SVGElement
+  | undefined
+  | (string | number | HTMLElement | SVGElement | undefined)[];
+
 export function useInnerHTML<T>({
   state,
 }: UseInnerHTMLProps<T>): (
   el: HTMLElement,
-  children: (state: T) => string | number | HTMLElement | SVGElement | (string | number | HTMLElement | SVGElement)[],
+  children: (state: T) => Children,
   condition?: ((state: T) => boolean) | boolean,
 ) => void;
 
 export function useInnerHTML(): (
   el: HTMLElement,
-  children: () => string | number | HTMLElement | SVGElement | (string | number | HTMLElement | SVGElement)[],
+  children: () => Children,
   condition?: (() => boolean) | boolean,
 ) => void;
 
@@ -29,10 +37,12 @@ export function useInnerHTML<T>(props?: UseInnerHTMLProps<T>) {
       el.innerHTML = '';
       if (_condition) {
         if (Array.isArray(_children)) {
-          _children.forEach((c) => {
-            if (c instanceof HTMLElement || c instanceof SVGElement) el.appendChild(c);
-            else el.appendChild(document.createTextNode(String(c)));
-          });
+          _children
+            .filter((c) => c)
+            .forEach((c) => {
+              if (c instanceof HTMLElement || c instanceof SVGElement) el.appendChild(c);
+              else el.appendChild(document.createTextNode(String(c)));
+            });
         } else if (_children instanceof HTMLElement || _children instanceof SVGElement) {
           el.appendChild(_children);
         } else {
